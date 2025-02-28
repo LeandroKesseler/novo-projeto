d4c7573dff85af2d5ae5673ed78b3566854a3cfd
Leandro K <leandrokesseler@gmail.com>
2025-02-28 20:34:55 -0300

    Menu
    Menu - creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JogosComponent } from './views/jogos/jogos.component'; /* Criei essa rota para subsdtituir o href */
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'jogos',
+    component: JogosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 /* Angular Materials */
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { JogosComponent } from './views/jogos/jogos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    JogosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSlideToggleModule, /* Adicionado da API */
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
