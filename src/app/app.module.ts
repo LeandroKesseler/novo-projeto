1434f1febc16c93e9acd4a49a54fbad9cd170044
Leandro K <leandrokesseler@gmail.com>
2025-02-28 19:24:02 -0300

    Novo projeto Angular
    Estudos com Angular. Neste projeto foi criado sem o standalone, assim a interface tradicional está com a pasta app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+/* Angular Materials */
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule, /* Adicionado da API */
+    MatToolbarModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
